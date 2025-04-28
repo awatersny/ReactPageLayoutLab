d6db29e1c813fb3ef8788e2b64b5702ef3897930
awatersny <awatersny@gmail.com>
2025-04-28 14:50:23 -0400

    Update code structure

--- src/components/EmployeeList.jsx
+++ src/components/EmployeeList.jsx
@@ -2,30 +2,12 @@ import React from 'react'
 import EmployeeListItem from './EmployeeListItem'
 
 const employees = [
-  {
-    name: "James King",
-    title: "President and CEO"
-  },
-  {
-    name: "Julie Taylor",
-    title: "VP of Marketing"
-  },
-  {
-    name: "Eugene Lee",
-    title: "CFO"
-  },
-  {
-    name: "John Williams",
-    title: "VP of Engineering"
-  },
-  {
-    name: "Ray Moore",
-    title: "VP of Sales"
-  },
-  {
-    name: "Paul Jones",
-    title: "QA Manager"
-  },
+  { name: "James King", title: "President and CEO" },
+  { name: "Julie Taylor", title: "VP of Marketing" },
+  { name: "Eugene Lee", title: "CFO" },
+  { name: "John Williams", title: "VP of Engineering" },
+  { name: "Ray Moore", title: "VP of Sales" },
+  { name: "Paul Jones", title: "QA Manager" },
 ]
 
 export default function EmployeeList() {
--- src/components/EmployeeListItem.jsx
+++ src/components/EmployeeListItem.jsx
@@ -3,12 +3,8 @@ import React from 'react'
 export default function EmployeeListItem(props) {
   return (
     <div class="employee-list-item">
-      <h4>
-        {props.name}
-      </h4>
-      <h5>
-        {props.title}
-      </h5>
+      <h4>{props.name}</h4>
+      <h5>{props.title}</h5>
     </div>
   )
 }
